2e3724e23fbf9f70aedb24f2f82cbb48ba585c42
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-10-19 18:40:38 -0500

    added query methods to DB class

--- index.js
+++ index.js
@@ -1,14 +1,85 @@
-const DB = require('./db/DB');
-
+const DB = require('./db');
+const { Department, Role, Employee } = require('./lib/Table');
+const inquirer = require('inquirer');
 
 // Test DB Class connection
 const init = async () => {
-    const db = new DB('test')
-    await db.connect();
-    await db.query('SELECT * FROM products;');
-    db.close();
+    const db = new DB('business_db')
 
+    // Connect to database
+    await db.connect();
+
+    // Test SELECT queries
+    // await db.tblAll(["id", "name"], "department")
+    // await db.allRoles();
+    // await db.allEmployees();
+
+    // Test INSERT queries
+    await db.insert("department", [{ name: "Test" }, { name: "New test" }])
+
+
+    db.close();
     return;
 }
 
+
+// Here we load the initial prompts with a series of options. The first option is provided for you.
+// function loadMainPrompts() {
+//     prompt([
+//         {
+//             type: "list",
+//             name: "choice",
+//             message: "What would you like to do?",
+//             choices: [
+//                 {
+//                     name: "View All Employees",
+//                     value: "VIEW_EMPLOYEES"
+//                 },
+
+//                 // add more options here
+//             ]
+//         }
+//     ]).then(res => {
+//         let choice = res.choice;
+//         // Call the appropriate function depending on what the user chose
+
+//         switch (choice) {
+//             case "VIEW_EMPLOYEES":
+//                 viewEmployees();
+//                 break;
+
+//             // add the other case statements here
+//         }
+//     }
+//     )
+// }
+
+/* ======= Controllers ============================================================ */
+
+// Here is a function which handles the first prompt option:  View all employees
+// function viewEmployees() {
+
+//     // Here we call the method in the db file for finding all employees.
+//     // we get the result back, and then display the result 
+//     db.findAllEmployees()
+//         .then(([rows]) => {
+//             let employees = rows;
+//             console.log("\n");
+//             console.table(employees);
+//         })
+//         .then(() => loadMainPrompts());
+// }
+
+
+/* ======= END Controllers ============================================================ */
+
+/* 
+  You will write lots of other functions here for the other prompt options.
+  Note that some prompts will require you to provide more prompts, and these 
+  may need functions of their own.
+*/
+
+
+
+// Everything starts here!
 init();
